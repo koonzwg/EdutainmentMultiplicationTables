05120120a2211a5b7cc787b498dd8335c70fc94f
koonzwg <koonzwg@gmail.com>
2024-03-18 13:09:40 -0500

    Initial Commit

---
+++ EdutainmentMultiplicationTables/EdutainmentMultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentMultiplicationTablesApp.swift
+//  EdutainmentMultiplicationTables
+//
+//  Created by William Koonz on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentMultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
